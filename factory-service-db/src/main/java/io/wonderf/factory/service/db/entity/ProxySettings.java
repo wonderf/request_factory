3006465db951dccbcde5ed2a50cb227e37fd91eb
Stanley <pobolinstas@gmail.com>
2023-05-21 11:30:00 +0500

    add entity

---
+++ factory-service-db/src/main/java/io/wonderf/factory/service/db/entity/ProxySettings.java
@@ -0,0 +1,35 @@
+package io.wonderf.factory.service.db.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "proxy_settings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProxySettings {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "short_name")
+    private String shortName;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "pass")
+    private String pass;
+
+    @Column(name = "ip")
+    private String ip;
+
+    @Column(name = "port")
+    private Integer port;
+}
