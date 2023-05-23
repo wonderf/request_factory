9b30b31948ba820ec9e3a9c6b54e11bbd6e9222e
Stanley <pobolinstas@gmail.com>
2023-05-23 22:55:38 +0500

    add entity scan

--- factory-service-api/src/main/java/io/wonderf/factory/service/api/Application.java
+++ factory-service-api/src/main/java/io/wonderf/factory/service/api/Application.java
@@ -1,9 +1,12 @@
 package io.wonderf.factory.service.api;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication()
+@SpringBootApplication
+@EntityScan("io.wonderf.factory.service.db.*")
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
